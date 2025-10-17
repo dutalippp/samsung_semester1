4561baef5f85661161f3ea6a62c3d795caed2dc2
dutalippp <aliefjlk4@gmail.com>
2025-10-17 08:00:41 +0700

    djangotutorial

---
+++ djangotutorial/polls/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("GAME FREE FIRE ADALAH GAME TERBAIK DI DUNIA, TIDAK ADA GAME TERBAIK SELAIN FREE FIRE#salambooyah")
